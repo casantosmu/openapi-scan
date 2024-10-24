67e35a481ee8341d1bf6d87ea61bcfa42056146d
Carlos Santos <casantosmu@gmail.com>
2024-10-24 23:48:09 +0200

    Add XO linter and remove Prettier/ESLint

--- cli.js
+++ cli.js
@@ -1,26 +1,27 @@
 #!/usr/bin/env node
 
-import consumers from "node:stream/consumers";
-import SwaggerParser from "@apidevtools/swagger-parser";
-import yaml from "js-yaml";
+import process from 'node:process';
+import consumers from 'node:stream/consumers';
+import SwaggerParser from '@apidevtools/swagger-parser';
+import yaml from 'js-yaml';
 
 const argv = process.argv.slice(2);
-const { stdin } = process;
+const {stdin} = process;
 
-const handleError = (error) => {
-  if (error) {
-    console.error(error.message);
-    process.exit(1);
-  }
+const handleError = error => {
+	if (error) {
+		console.error(error.message);
+		process.exit(1);
+	}
 };
 
 const definition = argv[0] ?? (await consumers.text(stdin));
 
 let api;
 try {
-  api = yaml.load(definition);
+	api = yaml.load(definition);
 } catch (error) {
-  handleError(new Error(`Invalid JSON or YAML: ${error.message}`));
+	handleError(new Error(`Invalid JSON or YAML: ${error.message}`));
 }
 
 SwaggerParser.validate(api, handleError);
